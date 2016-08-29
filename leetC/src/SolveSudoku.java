bae0bd0ce96ca811060e44429560aa90d6ef4ce1
sarahrenjun <renshuangjun_sx@iqiyi.pps>
2016-08-29 17:47:32 +0800

    更改了项目的目录结构

---
+++ leetC/src/SolveSudoku.java
@@ -0,0 +1,5 @@
+/**
+ * Created by renshuangjun_sx on 2016/8/29.
+ */
+public class SolveSudoku {
+}
--- test/src/TestComparator.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by renshuangjun_sx on 2016/8/12.
- */
-public class TestComparator {
-    public static void main(String[] args) {
-        System.out.println("e".compareTo("f"));
-    }
-}
